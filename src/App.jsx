1824a2033992110182c5e52aef8093287dfac99b
Illarion <superphantom88@gmail.com>
2023-11-23 22:20:50 +0600

    HomeWork

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,16 @@ import {
   Image,
   Link,
   NextUIProvider,
+  Tab,
+  Tabs,
 } from "@nextui-org/react";
 
 function App() {
-  return <div></div>;
+  return (
+    <Tabs>
+      <Tab></Tab>
+    </Tabs>
+  );
 }
-
-const UserCard = ({ name, age, ...props }) => (
-  <Card className="max-w-[400px]">
-    <CardHeader className="flex gap-3">
-      <Image
-        alt="nextui logo"
-        height={40}
-        radius="sm"
-        src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-        width={40}
-      />
-      <div className="flex flex-col">
-        <p className="text-md">{name}</p>
-      </div>
-    </CardHeader>
-    <Divider />
-    <CardBody>
-      <p className="text-small text-default-500">{age}</p>
-    </CardBody>
-    <Divider />
-    <CardFooter>
-      <Link
-        isExternal
-        showAnchorIcon
-        href="https://github.com/nextui-org/nextui"
-      >
-        Aslan loh
-      </Link>
-    </CardFooter>
-  </Card>
-);
 
 export default App;
